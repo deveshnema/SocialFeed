6e516404577cf8771090cb42a932b6d056e477ce
Devesh Nema <deveshnema@deveshs-macbook-air.local>
2018-05-20 23:15:35 -0700

    1) Added label for showing number of likes and comments 2) Added buttons for like, share, comment

--- SocialFeed/FeedViewController.swift
+++ SocialFeed/FeedViewController.swift
@@ -29,7 +29,7 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 300)
+        return CGSize(width: view.frame.width, height: 400)
     }
 }
 
@@ -85,6 +85,8 @@ class FeedCell : UICollectionViewCell {
         let textView = UITextView()
         textView.text = "Hi, this is batman"
         textView.font = UIFont.systemFont(ofSize: 14)
+        textView.isEditable = false
+        textView.isScrollEnabled = false
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
@@ -98,13 +100,77 @@ class FeedCell : UICollectionViewCell {
         return imageView
     }()
     
+    let likeCommentLabel : UILabel = {
+        let label = UILabel()
+        label.text = "12.8K Likes  45K Comments"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let separatorView : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let buttonsView : UIStackView = {
+        let view = UIStackView()
+        view.alignment = .center
+        view.axis = .horizontal
+        view.distribution = .fillEqually
+        view.spacing = 5
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let likeButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Like", for: .normal)
+        button.setTitleColor(UIColor.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0)
+        button.setImage(UIImage(named: "like"), for: .normal)
+        return button
+    }()
+    
+    let shareButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Share", for: .normal)
+        button.setTitleColor(UIColor.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0)
+        button.setImage(UIImage(named: "share"), for: .normal)
+        return button
+    }()
+    
+    let commentButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Comment", for: .normal)
+        button.setTitleColor(UIColor.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0)
+        button.setImage(UIImage(named: "comment"), for: .normal)
+        return button
+    }()
+    
     func setupViews() {
         backgroundColor = UIColor.yellow
         addSubview(nameLabel)
         addSubview(profileImageView)
         addSubview(statusTextView)
         addSubview(statusImageView)
+        addSubview(likeCommentLabel)
+        addSubview(separatorView)
+        addSubview(buttonsView)
         
+        buttonsView.addArrangedSubview(likeButton)
+        buttonsView.addArrangedSubview(commentButton)
+        buttonsView.addArrangedSubview(shareButton)
+        
+
         //Constraints for profileImageView
         profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
         profileImageView.trailingAnchor.constraint(equalTo: nameLabel.leadingAnchor, constant: -8).isActive = true
@@ -124,10 +190,26 @@ class FeedCell : UICollectionViewCell {
         //Constraints for statusImageView
         statusImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
         statusImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        statusImageView.topAnchor.constraint(equalTo: statusTextView.bottomAnchor, constant: 8).isActive = true
-        statusImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        statusImageView.topAnchor.constraint(equalTo: statusTextView.bottomAnchor, constant: 4).isActive = true
 
-       
+        //Constraints for likeCommentLabel
+        likeCommentLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12).isActive = true
+        likeCommentLabel.topAnchor.constraint(equalTo: statusImageView.bottomAnchor, constant: 8).isActive = true
+        likeCommentLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+        //Constraints for separatorView
+        separatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
+        separatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        separatorView.topAnchor.constraint(equalTo: likeCommentLabel.bottomAnchor, constant: 8).isActive = true
+
+        //Constraints for buttonsView
+        buttonsView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        buttonsView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        buttonsView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        buttonsView.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: 8).isActive = true
+        buttonsView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+        
     }
     
 }
